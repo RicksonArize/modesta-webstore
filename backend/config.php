38bc40dae03d6e79074e1073b6d221762a16d141
Rickson Arize <riksonluis@hotmail.com>
2025-11-13 13:49:55 -0300

    Implementação do Banco de Dados

--- backend/config.php
+++ backend/config.php
@@ -1,4 +1,13 @@
 <?php
+    // ini_set('display_errors', 1);
+    // error_reporting(1);
+
     header('Content-Type: charset=utf-8');
     session_start();
+
+    define("DB_HOST", "localhost");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
+    define("DB_SCHEMA", "modesta_webstore");
+
 ?>
---
+++ backend/db.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once './config.php';
+
+$db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_SCHEMA);
+
+if($db->connect_errno){
+    echo "Erro Banco de dados: {$db->connect_error}";
+    exit();
+}
+
